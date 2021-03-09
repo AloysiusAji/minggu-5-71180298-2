7bab30c53ccf527fc33bf6d12b967571d9e37253
AloysiusAji <73976886+AloysiusAji@users.noreply.github.com>
2021-03-10 00:29:32 +0700

    up

--- Soal Buatan sendiri minggu 5 71180298.py
+++ Soal Buatan sendiri minggu 5 71180298.py
@@ -4,22 +4,22 @@
 '''membuat program penghitungan volume bangun ruang menggunakan perulangan yang terdiri dari
 kubus = sisi^3, balok = p * l * t, bola = 4/3 * 3.14 * jari**3 '''
 
-def bangunRuang():
+def bangunRunag():
     def menu():
-        print("Penghitung Volume Bangun Ruang Sederhana")
-        print("1. Kubus")
-        print("2. Balok")
-        print("3. Bola")
-        print("4. Exit")
+        print("Penghitung Volume Bangun Ruang")
+        print("1.Kubus")
+        print("2.Balok")
+        print("3.Bola")
+        print("4.Keluar")
 
     def kubus(sisi):
         volumeKubus = sisi**3
         return volumeKubus
 
-    def balok(p, l, t):
+    def balok(p,l,t):
         volumeBalok = p * l * t
         return volumeBalok
-    
+
     def bola(jari):
         volumeBola = 4/3 * 3.14 * jari**3
         return volumeBola
@@ -27,23 +27,24 @@ def bangunRuang():
     loop = True
     while loop:
         menu()
-        pilihan = int(input("masukan pilihan anda: "))
+        pilihan = int(input("Masukan pilihan anda: "))
 
         if pilihan == 1:
-            sisi = int(input("Masukan panjang sisi kubus tersebut: "))
-            print("Volume kubusnya adalah",kubus(sisi),"cm^2s")
+            sisi = int(input("Masukan sisi kubus yang di inginkan: "))
+            print("Volume kubusnya adalah",kubus(sisi),"cm^2")
         elif pilihan == 2:
-            p = int(input("Masukan panjang balok tersebut: "))
-            l = int(input("Masukan lebar balok tersebut: "))
-            t = int(input("Masukan panjang tinggi balok tersebut: "))
-            print("Volume baloknya adalah",balok(p, l, t),"cm^2s")
+             p = int(input("Masukan panjang balok yang di inginkan: "))
+             l = int(input("Masukan lebar balok yang di inginkan: "))
+             t = int(input("Masukan tinggi balok yang di inginkan: "))
+             print("Volume baloknya adalah",balok(p,l,t),"cm^2")
         elif pilihan == 3:
-            jari = int(input("Masukan panjang jari-jari tersebut: "))
-            print("Volume bola adalah ",bola(jari),"cm^2")
+            jari = int(input("masukan Jari-Jari yang di inginkan: "))
+            print("Volume bolanya adalah",bola(jari),"cm^2")
         elif pilihan == 4:
             loop = False
-            print("Selesai...")
+            print("keluar...")
         else:
-            print("Inputan yang anda masukan salah")
+            print("Yang anda inputkan salah")
     return pilihan
-print (bangunRuang())
+print (bangunRunag())
+
